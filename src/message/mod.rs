3a2da8c551362c61340e11bdb1417502fdec6d2a
Ethan Jones <ethan@yasfu.net>
2021-05-09 03:12:41 -0600

    Message Mod fix, use command directly. Bump version & fix name

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,12 +1,15 @@
 use crate::user::User;
-
-pub(crate) mod command;
+use crate::channel::Channel;
+use crate::command::LakituCommand;
 
 pub trait Message {
-    fn get_contents(&self) -> String;
-    fn get_time(&self) -> int32;
+    fn get_time(&self) -> i64;
     fn get_owner(&self) -> Box<dyn User>;
+    fn get_channel(&self) -> Box<dyn Channel>;
+    fn get_contents(&self) -> &str;
+
+    fn is_private(&self) -> bool;
 
     fn is_command(&self) -> bool;
-    fn get_command(&self) -> Opt
+    fn get_command(&self) -> Option<Box<dyn LakituCommand>>;
 }
